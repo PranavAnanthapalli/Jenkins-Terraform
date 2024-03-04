b3d02b42e3eba0fbcc8a6c6c737975b3b65aad29
PranavAnanthapalli <112987297+PranavAnanthapalli@users.noreply.github.com>
2024-03-04 01:46:48 -0500

    Update test.tf

--- test.tf
+++ test.tf
@@ -13,11 +13,11 @@ terraform {
 
 
 provider "aws" {
-  region     = "us-east-1"
+  region     = "ca-central-1"
 }
 
 resource "aws_instance" "myec2" {
-   ami = "ami-09988af04120b3591"
+   ami = "ami-0156b61643fdfee5c"
    instance_type = "t2.micro"
    key_name = "rhel"
    tags = {
